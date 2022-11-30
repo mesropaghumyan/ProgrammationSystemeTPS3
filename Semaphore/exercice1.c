fba67fa660fdff1bf0ddfaf4d0481b3b43d57483
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2022-11-30 16:09:30 +0100

    create new dir for semaphore tp and add exercice 1

---
+++ Semaphore/exercice1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long long int N = 8000000000;
+
+int main(void) {
+
+    double temps;
+    clock_t start, end;
+
+    double i, A = 0, B = 0;
+
+    start = clock();
+
+    for(i = 1; i < N; i++) { A = A + (1/(17+i*i)); }
+
+    for(i = 1; i < N; i++) { B = B + (1/(A+i*i)); }
+
+    end = clock();
+
+    temps = ((double) (end-start)) / CLOCKS_PER_SEC;
+
+    printf("A = %lf ; B = %lf ; Temps = %lf\n", A, B, temps);
+
+    return 0;
+}
