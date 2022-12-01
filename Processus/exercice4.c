779d6b7dff1026c022ba26267684a437df9d91d2
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2022-12-01 13:45:10 +0100

    add exercice 4 of TP Processus

---
+++ Processus/exercice4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long long int N = 7200000000;
+
+int main(void) {
+
+    double s = 0;
+    double temps;
+    clock_t start, end;
+
+    start = clock();
+
+    for(double i = 1; i < N; i++) {
+        s = s + (1/i/i);
+    }
+
+    end = clock();
+
+    temps = ((double) (end-start)) / CLOCKS_PER_SEC;
+
+    printf("Résultat = %lf ; Temps = %lf\n", s, temps);
+
+}
